3f8a022709858e057fd4f06ce4edcff5377b3b4b
guonaihong <guonaihong@qq.com>
2020-02-03 20:06:17 +0800

    修改下空格

--- clop_test.go
+++ clop_test.go
@@ -16,22 +16,22 @@ type testAPI struct {
 func Test_API_bool(t *testing.T) {
 	type cat struct {
 		NumberNonblank bool `clop:"-b;--number-nonblank"
-							usage:"number nonempty output lines, overrides"`
+                             usage:"number nonempty output lines, overrides"`
 
 		ShowEnds bool `clop:"-E;--show-ends"
-						    usage:"display $ at end of each line"`
+                       usage:"display $ at end of each line"`
 
 		Number bool `clop:"-n;--number"
-						    usage:"number all output lines"`
+                     usage:"number all output lines"`
 
 		SqueezeBlank bool `clop:"-s;--squeeze-blank"
-						    usage:"suppress repeated empty output lines"`
+                           usage:"suppress repeated empty output lines"`
 
 		ShowTab bool `clop:"-T;--show-tabs"
-						    usage:"display TAB characters as ^I"`
+                      usage:"display TAB characters as ^I"`
 
 		ShowNonprinting bool `clop:"-v;--show-nonprinting"
-						    usage:"use ^ and M- notation, except for LFD and TAB" `
+                              usage:"use ^ and M- notation, except for LFD and TAB" `
 
 		Args []string `clop:"args"`
 	}
